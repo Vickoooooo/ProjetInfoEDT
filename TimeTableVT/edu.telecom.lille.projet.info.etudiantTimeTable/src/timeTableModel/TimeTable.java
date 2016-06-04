bf8c95403ecb122e3226472fb406875d780b0c78
Vickoooooo <vic_duruiss@orange.fr>
2016-06-04 18:54:46 +0200

    Réalisation class TimeTable

--- TimeTableVT/edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/TimeTable.java
+++ TimeTableVT/edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/TimeTable.java
@@ -21,12 +21,12 @@ public class TimeTable {
 	/**
 	 * Description of the property groupID.
 	 */
-	private String groupID = "";
+	//private String groupID;
 
 	/**
 	 * Description of the property timeTablesID.
 	 */
-	private int timeTablesID = 0;
+	private int timeTablesID;
 
 	// Start of user code (user defined attributes for TimeTable)
 
@@ -45,7 +45,7 @@ public class TimeTable {
 	 * Description of the method getTimeTable.
 	 * @param int 
 	 */
-	public void getTimeTable(Object int) {
+	//public void getTimeTable(Object int) {
 		// Start of user code for method getTimeTable
 		// End of user code
 	}
@@ -54,7 +54,7 @@ public class TimeTable {
 	 * Description of the method setTimeTable.
 	 * @param int 
 	 */
-	public void setTimeTable(Object int) {
+	//public void setTimeTable(Object int) {
 		// Start of user code for method setTimeTable
 		// End of user code
 	}
@@ -69,12 +69,15 @@ public class TimeTable {
 	public HashSet<Booking> getBookings() {
 		return this.bookings;
 	}
-
+	
+	public void setList_booking(HashSet<Booking> newList_booking) {
+		this.bookings = newList_booking;
+	}
 	/**
 	 * Returns groupID.
 	 * @return groupID 
 	 */
-	public String getGroupID() {
+	//public String getGroupID() {
 		return this.groupID;
 	}
 
@@ -82,7 +85,7 @@ public class TimeTable {
 	 * Sets a value to attribute groupID. 
 	 * @param newGroupID 
 	 */
-	public void setGroupID(String newGroupID) {
+	//public void setGroupID(String newGroupID) {
 		this.groupID = newGroupID;
 	}
 
